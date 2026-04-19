3948952cf546af7ee30a850344997df275e9eb02
Zev Brisk <shloimazevbrisk@gmail.com>
2026-04-18 20:27:58 -0400

    fix port

--- server.js
+++ server.js
@@ -396,6 +396,8 @@ app.post("/save-website", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
